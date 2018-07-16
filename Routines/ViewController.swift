98cb705a2a218acf7a944ab542eacc8de7dd2cfa
andremartingo <andre.martingo@gmail.com>
2018-07-16 15:48:47 +0100

    Bootstrap project

--- Routines/ViewController.swift
+++ Routines/ViewController.swift
@@ -1,25 +1,9 @@
-//
-//  ViewController.swift
-//  Routines
-//
-//  Created by André Martingo on 16/07/2018.
-//  Copyright © 2018 André Martingo. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
