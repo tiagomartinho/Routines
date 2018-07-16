19f63b4f148904a7d7f211cb3887a0ddd9d66e82
andremartingo <andre.martingo@gmail.com>
2018-07-16 16:31:51 +0100

    Listen to switch value changes

--- Routines/AppDelegate.swift
+++ Routines/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = AddRoutineViewController()
         window?.makeKeyAndVisible()
         return true
     }
--- Routines/ViewController.swift
+++ Routines/ViewController.swift
@@ -1,9 +1,47 @@
 import UIKit
 
-class ViewController: UIViewController {
+class AddRoutineViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view = AddRoutineView { state in
+            print(state)
+        }
+    }
+}
+
+class AddRoutineView: UIView {
+    
+    let notificationSwitch = UISwitch()
+    var action: ((Bool)->Void)?
+    
+    convenience init(action: @escaping (Bool)->Void) {
+        self.init()
+        self.action = action
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = .white
+        notificationSwitch.addTarget(self, action: #selector(switchDidChange), for: .valueChanged)
+        addSubview(notificationSwitch)
+    }
+    
+    @objc func switchDidChange(sender: UISwitch) {
+        action?(sender.isOn)
+    }
+    
+    func setupConstraints() {
+        notificationSwitch.translatesAutoresizingMaskIntoConstraints = false
+        notificationSwitch.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
     }
 }
