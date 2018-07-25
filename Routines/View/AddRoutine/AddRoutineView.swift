3968b2079d3d987238e31f61e001551958f31987
andremartingo <andre.martingo@gmail.com>
2018-07-25 17:06:01 +0100

    add table view to AddRoutineView

--- Routines/View/AddRoutine/AddRoutineView.swift
+++ Routines/View/AddRoutine/AddRoutineView.swift
@@ -2,7 +2,15 @@ import UIKit
 
 @IBDesignable
 class AddRoutineView: UIView {
-    let notificationSwitch = UISwitch()
+    private let reuseIdentifier = "Cell"
+
+    let settingsTableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = UIColor(red: 243.0 / 255, green: 243.0 / 255, blue: 243.0 / 255, alpha: 1)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+
     weak var delegate: AddRoutineViewDelegate?
 
     convenience init(delegate: AddRoutineViewDelegate?) {
@@ -12,26 +20,54 @@ class AddRoutineView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setupViews()
-        setupConstraints()
+        setupSettingsTableView()
     }
 
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func setupViews() {
-        backgroundColor = .white
-        notificationSwitch.addTarget(self, action: #selector(switchDidChange), for: .valueChanged)
-        addSubview(notificationSwitch)
+    func setupSettingsTableView() {
+        addSubview(settingsTableView)
+
+        settingsTableView.dataSource = self
+        settingsTableView.delegate = self
+//        settingsTableView.register(ActivateRoutineCell.self, forCellReuseIdentifier: reuseIdentifier)
+
+        settingsTableView.topAnchor.constraint(equalTo: topAnchor, constant: 0).isActive = true
+        settingsTableView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 0).isActive = true
+        settingsTableView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 0).isActive = true
+        settingsTableView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 0).isActive = true
     }
 
     @objc func switchDidChange(sender: UISwitch) {
         sender.isOn ? delegate?.routineOn() : delegate?.routineOff()
     }
+}
 
-    func setupConstraints() {
-        notificationSwitch.translatesAutoresizingMaskIntoConstraints = false
-        notificationSwitch.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 60).isActive = true
+extension AddRoutineView: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in _: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dequeueCell(in: tableView, index: indexPath)
+        return cell
+    }
+
+    func tableView(_: UITableView, estimatedHeightForRowAt _: IndexPath) -> CGFloat {
+        return 80
+    }
+
+    private func dequeueCell(in tableView: UITableView, index _: IndexPath) -> UITableViewCell {
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) as? ActivateRoutineCell {
+//            cell.notificationSwitch.addTarget(self, action: #selector(switchDidChange), for: .valueChanged)
+//            return cell
+//        }
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
     }
 }
