f5d28938cbd0182bed3a546190ebaaaf7d026d03
andremartingo <andre.martingo@gmail.com>
2018-08-02 16:45:00 +0100

    Create Presenter for ViewController

---
+++ Routines/Presenter/DetailsRoutinePresenter.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+class DetailsRoutinePresenter {
+    private var notificationScheduler: NotificationScheduler
+    private var repository: RoutineRepository
+
+    public init(notificationScheduler: NotificationScheduler,
+                repository: RoutineRepository) {
+        self.notificationScheduler = notificationScheduler
+        self.repository = repository
+    }
+
+    func createRoutine(name: String, alarm: Bool, date: Date, frequency: Frequency) {
+        let routine = Routine(name: name, alarm: alarm, date: date, frequency: frequency)
+        if alarm {
+            createNotificaton(id: routine.getId(), name: name, on: date, frequency: frequency)
+        }
+    }
+
+    private func createNotificaton(id: String, name: String, on date: Date, frequency: Frequency?) {
+        notificationScheduler.createNotificationOnDate(id: id, name: name, date: date, frequency: frequency)
+        notificationScheduler.getAllNotifications()
+    }
+}
